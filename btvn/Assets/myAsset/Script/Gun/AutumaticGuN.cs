3ef64a3425d77576773f95543786c58f8df2de02
Thang-900 <hmanhthang41@gmail.com>
2025-09-05 18:02:22 +0700

    4.5

---
+++ btvn/Assets/myAsset/Script/Gun/AutumaticGuN.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AutomaticShooting : MonoBehaviour
+{
+    public Animator anim;
+    public int rpm;
+    public AudioSource shootSound;
+    public GameObject hitMarkerPrefab;
+    public Camera aimingCamera;
+    public LayerMask layerMask;
+    public UnityEvent onShoot;
+
+    private float lastShoot;
+    private float interval;
+
+    private void Start()
+    {
+        interval = 60f / rpm;
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            UpdateFiring();
+        }
+    }
+    private void UpdateFiring()
+    {
+        if(Time.time-lastShoot>=interval)
+        {
+            Shoot();
+            lastShoot = Time.time;
+        }
+    }
+    private void Shoot()
+    {
+        shootSound.Play();
+        PerformRayCasting();
+        onShoot.Invoke();
+    }
+    private void PerformRayCasting()
+    {
+        Ray aimingRay=new Ray(aimingCamera.transform.position
+            ,aimingCamera.transform.forward);
+        if(Physics.Raycast(aimingRay,out RaycastHit hitInfo,1000f,layerMask))
+        {
+            Quaternion effectRotation = Quaternion.LookRotation(hitInfo.normal);
+            Instantiate(hitMarkerPrefab, hitInfo.point,
+                effectRotation);
+        }
+    }
+}
--- btvn/Assets/myAsset/Script/Gun/grenadeBullet.cs
+++ btvn/Assets/myAsset/Script/Gun/grenadeBullet.cs
@@ -5,11 +5,32 @@ using UnityEngine;
 public class grenadeBullet : MonoBehaviour
 {
     public GameObject explosionEffect;
+    public float explosionRadius = 5f;
+    public float explosionForce;
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision collision)
     {
         Instantiate(explosionEffect, transform.position, transform.rotation);
         Destroy(gameObject);
+        BlowObject();
+    }
+    private void BlowObject()
+    {
+        Collider[] affectedObjects=Physics.OverlapSphere(transform.position,explosionRadius);
+        foreach(Collider obj in affectedObjects)
+        {
+           if(obj.GetComponent<Rigidbody>()!=null)
+            {
+                obj.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, 
+                    transform.position, explosionRadius, 1,
+                    ForceMode.Impulse);
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
 
